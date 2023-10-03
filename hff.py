89ca6920bf355669af8f8119125aebf11536d805
Priscila Kamiri <priscilla.waihiga@gmail.com>
2023-10-03 14:04:53 +0300

    first commit

--- hff.py
+++
@@ -1,6 +0,0 @@
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:waihiga9/Hamoye_Internship_D_Stotytelling.git
-git push -u origin main
